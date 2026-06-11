456685d44211ce64c61b88a2757e345fde0ceda0
adit-sinha <adit.sinha05@gmail.com>
2026-06-11 14:55:07 -0600

    backend fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/AI-Calorie-Tracker/",
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-calorie-tracker-wush.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
